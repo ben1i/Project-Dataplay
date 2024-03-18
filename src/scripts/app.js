ea158225578660d969c3c79663f7c2c5f4282feb
Benjamin <benjamin.brihaye@student.heaj.be>
2024-03-18 14:00:10 +0100

    Image + JS
    Ajout des images de base des posters + début du code JavaScript

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,68 @@
 "use strict"
+
+var score = 10;
+var posterContainer = document.querySelector('.test__img');
+var posterImage = posterContainer.querySelector('img');
+var posters = ["alien.jpg", "blairwitch.jpg", "carrie.jpg", "dawnofthedead.jpg", "elmstreet.jpg", "exorcist.jpg", "halloween.jpg", "hellraiser.jpg", "it.jpg", "jaws.jpg", "nightofthelivingdead.jpg", "omen.jpg", "psycho.jpg", "rosemarysbaby.jpg", "shining.jpg", "suspiria.jpg", "texaschainsawmassacre.jpg", "theevildead.jpg", "thering.jpg", "thething.jpg"];
+
+var testText = document.querySelector('.test__text');
+
+function getRandomIntInclusive(min,max) {
+    min= Math.ceil(min);
+    max=Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function choisirPosterAleatoire() {
+    const cheminDossier = "./assets/images/basic";
+     // Liste des noms de fichiers de vos images
+    const indexAleatoire = Math.floor(Math.random() * posters.length);
+    const posterChoisi = posters[indexAleatoire];
+    const cheminPoster = `${cheminDossier}/${posterChoisi}`;
+
+    return cheminPoster;
+}
+
+function choisirPosterShinyAleatoire() {
+    const cheminDossier = "./assets/images/shiny";
+    const posters = ["alien.jpg", "poster2.jpg", "poster3.jpg"]; // Liste des noms de fichiers de vos images
+
+    const indexAleatoire = Math.floor(Math.random() * posters.length);
+    const posterChoisi = posters[indexAleatoire];
+    const cheminPoster = `${cheminDossier}/${posterChoisi}`;
+
+    return cheminPoster;
+}
+
+function afficherPoster(cheminPoster) {
+    posterImage.src = cheminPoster;
+}
+
+function afficherShinyPoster(cheminPoster) {
+    posterImage.src = cheminPoster;
+}
+
+if (score === 10) {
+    const cheminPoster = choisirPosterAleatoire();
+    afficherPoster(cheminPoster);
+    console.log(cheminPoster)
+    if (cheminPoster === "./assets/images/basic/alien.jpg") {
+        localStorage.setItem("Alien", "Y");
+        testText.classList.remove('hidden');
+        posterImage.alt = "Poster de Alien (1979)"
+    }
+}
+
+if (score === 20) {
+    const cheminPoster = choisirPosterShinyAleatoire();
+    afficherShinyPoster(cheminPoster);
+    if (cheminPoster === "./assets/images/shiny/alien.jpg") {
+        localStorage.setItem("AlienShiny", "Y");
+    }
+}
+
+var Alien = localStorage.getItem("Alien")
+
+if (Alien === "Y") {
+    posters.slice('alien.jpg');
+}

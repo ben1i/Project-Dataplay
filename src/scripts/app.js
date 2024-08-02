9986dfc0d08e2e56e3d391be74c8a849f1f67a6b
Benjamin <benjamin.brihaye@student.heaj.be>
2024-08-02 16:01:28 +0200

    JSON + HTML + JS
    Fin des données JSON + Fin du HTML + Début du JS (fetch)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,21 @@
 "use strict"
+
+var gamePage = document.querySelector('.dataplay__game');
+
+fetch('assets/json/data.json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(data => {
+        
+        const gameMaps = data.gamemaps;
+        console.log(gameMaps);
+
+        /*const sanAndreas = gameMaps[0];
+        console.log(sanAndreas);
+
+        let sanAndreasDiv = document.createElement('div');
+        sanAndreasDiv.className = 'game__map';
+        sanAndreasDiv.style.width = sanAndreas[1]*1000;
+        gamePage.appendChild(sanAndreasDiv);*/
+    });
